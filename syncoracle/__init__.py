d1a47297e432cf8c0a35b958e54c5b5305a847e6
h-rub <rubio@loopgk.com>
2023-10-17 16:14:24 -0600

    Versión 21_11

--- syncoracle/__init__.py
+++ syncoracle/__init__.py
@@ -6,7 +6,7 @@ import cx_Oracle
 try:
     if sys.platform.startswith("darwin"):
         lib_dir = os.path.join(os.environ.get("HOME"), "Downloads",
-                               "instantclient_19_8")
+                               "instantclient_21_11")
         cx_Oracle.init_oracle_client(lib_dir=lib_dir)
     elif sys.platform.startswith("win32"):
         lib_dir=r"C:\oracle\instantclient_21_11"
